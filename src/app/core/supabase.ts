f1a4698706d454014e6ab99ceb8bc0c8229aca38
Gauri Dhakad <gauri@Gauris-Laptop.local>
2026-01-04 19:27:12 +0530

    fix(supabase):valid https url added

--- src/app/core/supabase.ts
+++ src/app/core/supabase.ts
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient('backend server 1', 'backened server 2');
+export const supabase = createClient(
+  'https://supabase.com',
+  'backened server 2'
+);
