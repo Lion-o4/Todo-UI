e5af13d9be46224cb9d2de052a293d900f57280e
Gauri Dhakad <gauri@Gauris-Laptop.local>
2026-01-04 19:18:05 +0530

    feat(info-card,dashboard,supabase):complete info-card

---
+++ src/app/core/supabase.ts
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient('backend server 1', 'backened server 2');
--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
 import { InfoCard } from '../../shared/info-card/info-card';
+import { supabase } from '../../core/supabase';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [InfoCard],
+  imports: [InfoCard, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  cards = [
+    {
+      title: 'Total Tasks',
+      value: 4,
+      sub: '2 pending',
+      color: 'blue',
+    },
+    {
+      title: 'In Progress',
+      value: 1,
+      sub: 'Active tasks',
+      color: 'orange',
+    },
+    {
+      title: 'Completed',
+      value: 1,
+      sub: '25% completion rate',
+      color: 'green',
+    },
+  ];
+
+  register() {
+    this.signup('gauridhakad9893@gmail.com', 'Gauri79@');
+  }
+  async signup(email: string, password: string) {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) {
+      alert(error.message);
+    } else {
+      console.log(data);
+
+      alert('Registration successful');
+    }
+  }
+  loguser() {
+    this.login('gauridhakad9893@gmail.com', 'Gauri79@');
+  }
+  async login(email: string, password: string) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      console.log(data);
+
+      alert('Login successful');
+    }
+  }
+}
